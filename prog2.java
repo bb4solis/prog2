efe2cc165ba3b62282e37e49d59e793ad7693ba0
Brenda Solis <brendab4solis@gmail.com>
2020-02-13 18:33:13 -0800

    skeleton buiding

---
+++ prog2.java
@@ -0,0 +1,34 @@
+/*Brenda Solis
+  Class CEC 274-05
+  Project: prog 2- Solitaire Prime
+  Due Date: March 3, 2020
+*/
+
+/**
+*/
+public class Card{
+    private char suit;
+    private char rank;
+
+    public Card(char r,char s)
+    {
+        char suit=s;
+        char rank=r;
+    }
+    public void display()
+    {
+
+    }
+    public int getValue()
+    {
+
+    }
+    public char getRank
+    {
+
+    }
+    public char getSuite()
+    {
+        
+    }
+}
