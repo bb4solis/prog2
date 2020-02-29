430653fee56a8a74858fe23dfd27175083d09fb6
Brenda Solis <brendab4solis@gmail.com>
2020-02-28 22:49:30 -0800

    ALMOST THERE HO!

--- prog2.java
+++ prog2.java
@@ -4,7 +4,7 @@
   Due Date: March 3, 2020
 */
 
-
+import java.util.Scanner;
 import java.util.Random;
 //We will build the card first 
 
@@ -96,3 +96,54 @@ public class Deck{
         return  52 - top;
     }
 }
+
+public static int  menu()
+{   Scanner in= new Scanner(System.in);
+    int choice;
+    System.out.println("------Welcome to Solitare Prime------");
+    System.out.print("1)New Deck\n 2)Display Deck\n 3)Shuffle Deck\n");
+    System.out.print("4)Play Solitare Prime\n 5)Exit\n 6)Simulate 1000 times \n");
+    choice=in.nextInt();
+    return choice; 
+}
+public static boolean isPrime(int x)
+{   boolean answer=false;
+    if(isPrime(x)==true) answer=true;
+    return answer;
+}
+public static void main(String[] Args)
+{   int choice=menu();
+    do
+    {
+    }while(choice!=6);
+}
+/**
+ * switch(choice)
+    {
+        case 1:
+            Deck myDeck= new Deck();
+            break;
+        case 2:
+                
+        break;
+        case 3:
+
+            break;
+        case 4:
+            break;
+        case 5:
+            break;
+        case 6:
+            int wins=0m losses=0;
+            for(int i=0; i < 1000; i++)
+            {
+                myDeck = new Deck();
+                myDeck.shuffle();
+                if(playSolitarePrime(myDeck)==true) wins++;
+                else losses++;
+            }
+            System.out.println("In 1000 games, you won:"+wins+" and lost:"+ losses);
+            break;
+        default:
+            System.out.print("INVALID INPUT GRRR.");
+ */
